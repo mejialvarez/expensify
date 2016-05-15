1c787470482059fecd0b3b5ae475a60e96e49c17
Hamilton Alvarez Mejia <alvarezhamilton@gmail.com>
2016-05-14 22:16:26 -0500

    Change from SQLite to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160515012842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
